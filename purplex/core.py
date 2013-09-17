069c20143be9e2f904421cc90e3d6bf37070214b
Michael Tom-Wing <mtomwing@gmail.com>
2013-09-16 22:48:51 -0700

    Added 1-indexed line number, and line pos attributes to tokens.

--- purplex/core.py
+++ purplex/core.py
@@ -25,27 +25,38 @@ class Lexer(metaclass=LexerBase):
     def __init__(self, input_text):
         self.input_text = input_text
         self.input_pos = 0
+        self.line_num = 1
+        self.line_pos = 1
 
     def __iter__(self):
         while not self.done():
             yield self.token()
 
-    def consume(self, n):
-        self.input_pos += n
+    def consume(self, token):
+        self.input_pos += len(token)
+        self.line_num += token.value.count('\n')
+
+        newline_idx = token.value.rfind('\n')
+        if newline_idx >= 0:
+            self.line_pos = len(token.value) - newline_idx
+        else:
+            self.line_pos += len(token.value)
 
     def token(self):
         matches = []
         for name, token_defn in self.tokens.items():
             match = token_defn.regexp.match(self.input_text, self.input_pos)
             if match:
-                token = Token(name, match.group(0), token_defn)
+                token = Token(name, match.group(0), token_defn,
+                              self.line_num, self.line_pos)
                 matches.append(token)
 
         s_matches = sorted(matches, key=lambda t: len(t), reverse=True)
 
         if s_matches:
             token = s_matches[0]
-            self.consume(len(token))
+            self.consume(token)
+
             if hasattr(self, 'on_{}'.format(token.name)):
                 getattr(self, 'on_{}'.format(token.name))(token)
             return token
--- purplex/token.py
+++ purplex/token.py
@@ -7,10 +7,19 @@ class TokenDef(object):
 
 
 class Token(object):
-    def __init__(self, name, value, defn):
+    def __init__(self, name, value, defn, line_num, line_pos):
         self.name = name
         self.value = value
         self.defn = defn
+        self.line_num = line_num
+        self.line_pos = line_pos
+
+        # For compatibility with yacc
+        self.type = name
+
+    def __str__(self):
+        return '{}({})<line {}, col {}>'.format(self.name, repr(self.value),
+                                                self.line_num, self.line_pos)
 
     def __len__(self):
         return len(self.value)
